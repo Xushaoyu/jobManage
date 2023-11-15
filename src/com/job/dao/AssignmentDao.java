6f120461b03d33e54637d4fbf1c486183756e285
lvyaxin <1339335924@qq.com>
2023-11-15 16:45:56 +0800

    老师添加作业业务代码完成

--- src/com/job/dao/AssignmentDao.java
+++ src/com/job/dao/AssignmentDao.java
@@ -2,7 +2,6 @@ package com.job.dao;
 
 import com.job.model.Assignment;
 import com.job.util.DBUtil;
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -19,18 +18,19 @@ public class AssignmentDao {
         Date date = new java.sql.Date(System.currentTimeMillis());
         PreparedStatement preparedStatement = connection
                 .prepareStatement("insert into assignments(assignment_title, assignment_description, assignment_deadline, " +
-                        "assignment_subject, assignment_class, tea_id, as_is_delete, update_time, create_time) " +
-                        "values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                        "assignment_subject, assignment_class, tea_id, update_time, create_time) " +
+                        "values (?, ?, ?, ?, ?, ?, ?, ?)");
         // Parameters start with 1
         preparedStatement.setString(1, assignment.getAssignmentTitle());
         preparedStatement.setString(2, assignment.getAssignmentDescription());
-        preparedStatement.setDate(3, (Date) assignment.getAssignmentDeadLine());
+        //把java的date转成sql的date,会丢失时分秒
+        java.sql.Date deadLineDate = new java.sql.Date(assignment.getAssignmentDeadLine().getTime());
+        preparedStatement.setDate(3, deadLineDate);
         preparedStatement.setString(4, assignment.getAssignmentSubject());
         preparedStatement.setString(5, assignment.getAssignmentClass());
         preparedStatement.setInt(6, assignment.getTeaId());
-
+        preparedStatement.setDate(7, date);
         preparedStatement.setDate(8, date);
-        preparedStatement.setDate(9, date);
         preparedStatement.executeUpdate();
     }
 }
