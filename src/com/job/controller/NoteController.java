24613f5de291b1a26630b494e22ec1823617f46a
lvyaxin <1339335924@qq.com>
2023-12-22 11:09:48 +0800

    完成笔记的展示，添加，删除

--- src/com/job/controller/NoteController.java
+++ src/com/job/controller/NoteController.java
@@ -1,8 +1,10 @@
 package com.job.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.job.dao.CourseDao;
 import com.job.dao.NoteDao;
+import com.job.dao.StudentDao;
 import com.job.model.Course;
 import com.job.model.Note;
 import com.job.util.Common;
@@ -41,12 +43,16 @@ public class NoteController  extends BaseController{
         引入接口使用的ORM操作对象
      */
     private final NoteDao noteDao;
+    private final StudentDao studentDao;
 
 
     public NoteController() {
         super();
         this.noteDao = new NoteDao();
+        this.studentDao = new StudentDao();
         this.urlMethodMap.put("queryNote", "GET");
+        this.urlMethodMap.put("deleteNote", "POST");
+        this.urlMethodMap.put("saveNote", "POST");
         super.urlMethodMap = urlMethodMap;
     }
 
@@ -61,5 +67,28 @@ public class NoteController  extends BaseController{
         //输出到浏览器
         ResponseData responseData = new ResponseData();
         responseData.writeResponseData(resp, notes);
+    }
+
+    public void deleteNote(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+        //拿到id
+        int noteId = Integer.parseInt(req.getParameter("noteId"));
+        //调用DAO层拿结果
+        int count = noteDao.deleteNote(noteId);
+        //输出到浏览器
+        ResponseData responseData = new ResponseData();
+        responseData.writeResponseData(resp, String.valueOf(count));
+    }
+
+    public void saveNote(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+        //拿到id
+        String[] userInfo = Common.getUserInfoFromCookies(req);
+        assert userInfo != null;
+        int studentId = Integer.parseInt(userInfo[1]);
+        String noteContent = req.getParameter("noteContent");
+        //调用DAO层拿结果
+        int count = noteDao.saveNote(studentId, noteContent);
+        //输出到浏览器
+        ResponseData responseData = new ResponseData();
+        responseData.writeResponseData(resp, String.valueOf(count));
     }
 }
